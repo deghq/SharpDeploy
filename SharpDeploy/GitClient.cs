3f54df6d8a95038cb60abc6d7a83357acf4e868b
Ian Escarro <ian.escarro@gmail.com>
2020-07-05 22:56:31 +0800

    Feature git diff head default

--- SharpDeploy/GitClient.cs
+++ SharpDeploy/GitClient.cs
@@ -13,17 +13,26 @@ namespace SharpDeploy
             repo = new Repository(path);
         }
         
+        public List<string> DiffFiles(string shaFrom)
+        {
+            return DiffFiles(shaFrom, "");
+        }
+        
         public List<string> DiffFiles(string shaFrom, string shaTo)
         {
             var files = new List<string>();
             
-//            var from = repo.Head.Tip.Tree;
-//            var to = DiffTargets.WorkingDirectory;
+            var from = repo.Lookup<Commit>(shaFrom).Tree;
+//            Tree to = DiffTargets.WorkingDirectory;
+            TreeChanges treeChanges = null;
+            if (shaTo != "") {
+//                to = repo.Lookup<Commit>(shaTo).Tree;
+                treeChanges = repo.Diff.Compare<TreeChanges>(from, repo.Lookup<Commit>(shaTo).Tree);
+            } else {
+                treeChanges = repo.Diff.Compare<TreeChanges>(from, repo.Head.Tip.Tree);
+            }
             
-            var from = repo.Lookup<Commit>(shaFrom);
-            var to = repo.Lookup<Commit>(shaTo);
-            
-            foreach (TreeEntryChanges c in repo.Diff.Compare<TreeChanges>(from.Tree, to.Tree)) {
+            foreach (TreeEntryChanges c in treeChanges) {
                 files.Add(c.Path);
             }
             return files;
